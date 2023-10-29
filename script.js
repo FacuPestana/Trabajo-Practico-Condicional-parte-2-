04959888bed02dceec9d77bbe8700c5f4c2e7b67
Facundo A. Pestana <136341998+FacuPestana@users.noreply.github.com>
2023-10-29 20:28:47 -0300

    Version 1.0
    No lo termine me tira errores a cada rato

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+= (Se le asiga el valor a algo)
+== (Es igual a algo)
+<= (Mayor igual)
+=> (Menor igual)
+!= (Distinto)
+&& = y
+*/
+
+
+
+/* Punto 1
+prompt("Ingresa tu nombre")
+if (nombre == "Santino" || "Nahuel") {
+  alert("Bienvenido Señor, es un placer tenerte aquí.");
+} else {
+  alert("Bienvenido, " + nombre + "!Gracias por unirte a nosotros.");
+}
+
+*/
+/*Punto 2: Crea un programa que pida la edad del usuario y, si es mayor o igual a 18, muestra un mensaje indicando que es mayor de edad; de lo contrario, muestra un mensaje indicando que es menor de 
+edad.Crea un programa que pida la edad del usuario y, si es mayor o igual a 18, muestra un mensaje indicando que es mayor de edad; de lo contrario, muestra un mensaje indicando que es menor de edad.*/
+let edadUsuario
+prompt('Ingrese su edad') 
+if (edadUsuario) >= 18 
+alert{'Eres mayor de edad'} 
+
+else {'Sos menor de edad'}
+
+/*Punto 3
+let CategoriaEdad 
+prompt('Ingrese su edad')
+
+//Punto 4
+alert (DiaLaboral(prompt))
+if ((DiaLaboral) = 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'){
+    return 'Hoy se trabaja'} 
+  else {'Tenes el dia para descansar'}
+*/ 
