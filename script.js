4cdeeaf1a80d5ea11f0cfb56eced43dcc7726674
Facundo A. Pestana <136341998+FacuPestana@users.noreply.github.com>
2023-10-30 15:43:13 -0300

    Ultima Version
    Trabajo terminado

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 => (Menor igual)
 != (Distinto)
 && = y
+|| = O
 */
 
 
@@ -36,7 +37,7 @@ alert(DiaLaboral(prompt))
 if ((DiaLaboral) = 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes') {
   return 'Hoy se trabaja'
 }
-else { 'Tenes el dia para descansar'}
+else { 'Tenes el dia para descansar' }
 
 //Punto 5
 
@@ -48,14 +49,67 @@ else { 'Acceso denegado'}
 
 //Punto 6 
 let numero = Number(prompt('Ingresa un numero'))
-function multiplo(numero){
-  return numero %5 === 0
+function multiplo(numero) {
+  return numero % 5 === 0
 }
 
 //Punto 7
 let Descuento = Number(prompt)('Ingresa tu edad')
-function Descuento1(){
-    if (Descuento > 65) alert{
-      'Perfecto,tenes un descuento del 10%'
-    }
+function Descuento1(alert) {
+  if (Descuento > 65) {
+    'Perfecto,tenes un descuento del 10%'
+  }
+}
+
+//Punto 8 
+let Nota = Number(prompt)('Ingresa tu nota')
+function Nota1(alert) {
+  if (Nota >= 8) {
+    'Felicitaciones tu nota es de MS'
+  }
+  if (Nota >= 60) {
+    'Felicitaciones tu nota es una S, aprobaste, pero justito'
+  }
+
+  else (Nota <= 5); {
+    'Lamento decirte que tendras que venir a hacer el periodo de intensificacion'
+  }
+}
+
+//Punto 9
+
+let Lunes = 1
+let Martes = 2
+let Miercoles = 3
+let Jueves = 4
+let Viernes = 5
+let Sabado = 6
+let Domingo = 7
+function dia(x){ x=Number(prompt('Ingrese numero'))
+} 
+
+//Punto 10
+
+año = (prompt('Ingrese un año:')) 
+if ((año % 400 == 0) || (año % 4 == 0) && (año % 100 != 0)){
+  alert ('Este año es bisiesto')
+} else {
+  alert ('Este año NO es bisiesto')
+}
+
+//Punto 11    
+function compararCadenas(cadena1, cadena2) {
+  if (cadena1 === cadena2) {
+      console.log(cadena1 + 'y' + cadena2 + ' son iguales.');
+  } else {
+    console.log(cadena1 + 'y' + cadena2 + ' no son iguales.');
+  }
+}
+
+//12
+
+function calcularEdad(añoNacimiento) {
+  const añoActual = new Date().getFullYear(); // Vi en un video como lo podia usar entonces la implemento, Gracias
+  const edad = añoActual - añoNacimiento;
+  return edad;
 }
