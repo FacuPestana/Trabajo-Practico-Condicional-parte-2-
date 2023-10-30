cbe18d3c51494d65ce257362a4a3421be3885aee
alumno <alumno@Instituto.local>
2023-10-30 10:59:15 -0300

    script.js 30/10
    No añadi nada solo revise algunas cosas

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
 
 
-/* Punto 1
+// Punto 1
 prompt("Ingresa tu nombre")
 if (nombre == "Santino" || "Nahuel") {
   alert("Bienvenido Señor, es un placer tenerte aquí.");
@@ -17,23 +17,35 @@ if (nombre == "Santino" || "Nahuel") {
   alert("Bienvenido, " + nombre + "!Gracias por unirte a nosotros.");
 }
 
-*/
 /*Punto 2: Crea un programa que pida la edad del usuario y, si es mayor o igual a 18, muestra un mensaje indicando que es mayor de edad; de lo contrario, muestra un mensaje indicando que es menor de 
 edad.Crea un programa que pida la edad del usuario y, si es mayor o igual a 18, muestra un mensaje indicando que es mayor de edad; de lo contrario, muestra un mensaje indicando que es menor de edad.*/
 let edadUsuario
-prompt('Ingrese su edad') 
-if (edadUsuario) >= 18 
-alert{'Eres mayor de edad'} 
+prompt('Ingrese su edad')
+if (edadUsuario >= 18) {
+  alert('Usted puede ingresar')
+}
+else { ('Usted es menor no puede Ingresar') }
 
-else {'Sos menor de edad'}
 
-/*Punto 3
-let CategoriaEdad 
+//Punto 3
+let CategoriaEdad
 prompt('Ingrese su edad')
 
 //Punto 4
-alert (DiaLaboral(prompt))
-if ((DiaLaboral) = 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'){
-    return 'Hoy se trabaja'} 
-  else {'Tenes el dia para descansar'}
-*/ 
+alert(DiaLaboral(prompt))
+if ((DiaLaboral) = 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes') {
+  return 'Hoy se trabaja'
+}
+else { 'Tenes el dia para descansar'}
+
+//Punto 5
+
+alert(Contraseña(prompt))
+if ((Contraseña) = 'Secreto') {
+  return 'Acceso Concedido'
+}
+else { 'Acceso denegado'}
+
+//Punto 6 
+let numero = Number(prompt('Ingresa un numero'))
+function
